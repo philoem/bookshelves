4e3afdf5871e9cf9c9304343ad74904fa00b26a3
philoem <philoem@nexgate.ch>
2018-09-24 20:23:51 +0200

    gestion connexion + authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyB_h0LJtP99_mMdlF2LH6KDfYWcGQ8YijM",
+      authDomain: "bookshelves-1a4ef.firebaseapp.com",
+      databaseURL: "https://bookshelves-1a4ef.firebaseio.com",
+      projectId: "bookshelves-1a4ef",
+      storageBucket: "bookshelves-1a4ef.appspot.com",
+      messagingSenderId: "648530306414"
+    };
+    firebase.initializeApp(config);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
@@ -18,9 +19,11 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
   { path: 'auth/signup', component: SignupComponent },
   { path: 'auth/signin', component: SigninComponent },
-  { path: 'books', component: BookListComponent },
-  { path: 'books/new', component: BookFormComponent },
-  { path: 'books/view/:id', component: SingleBookComponent },
+  { path: 'books', canActivate: [AuthGuardService], component: BookListComponent },
+  { path: 'books/new', canActivate: [AuthGuardService],component: BookFormComponent },
+  { path: 'books/view/:id', canActivate: [AuthGuardService],component: SingleBookComponent },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: '**', redirectTo: 'books' }
 
 ];
 
